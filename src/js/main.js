137dec25425d7bf27d657bf28c646895e2ab24bb
emilymeng <eeng@seattletimes.com>
2018-08-28 19:20:10 -0700

    init

---
+++ src/js/main.js
@@ -0,0 +1,94 @@
+//load our custom elements
+require("component-leaflet-map");
+require("component-responsive-frame");
+
+//get access to Leaflet and the map
+var element = document.querySelector("leaflet-map");
+var L = element.leaflet;
+var map = element.map;
+
+var ich = require("icanhaz");
+var templateFile = require("./_popup.html");
+ich.addTemplate("popup", templateFile);
+
+var data = require("./homeless_schoolD.geo.json");
+
+
+var mapElement = document.querySelector("leaflet-map");
+
+if (mapElement) {
+  var L = mapElement.leaflet;
+  var map = mapElement.map;
+
+  map.scrollWheelZoom.disable();
+
+  var focused = false;
+
+  var all = "changeHome";
+
+  var commafy = s => (s*1).toLocaleString().replace(/\.0+$/, "");
+
+   data.features.forEach(function(f) {
+	["changeHome", "changeAll"].forEach(function(prop) {
+		f.properties[prop] = (f.properties[prop] * 100).toFixed(1);
+	});
+	});
+
+var onEachFeature = function(feature, layer) {
+  layer.bindPopup(ich.popup(feature.properties))
+  layer.on({
+  	     mouseover: function(e) {
+        layer.setStyle({ weight: 2.5, fillOpacity: .8 });
+      },
+      mouseout: function(e) {
+        if (focused && focused == layer) { return }
+        layer.setStyle({ weight: 1.5, fillOpacity: 0.6
+         });
+      }
+    });
+};
+
+var getColor = function(d) {
+    var value = d[all];
+    if (typeof value == "string") {
+      value = Number(value.replace(/,/, ""));
+    }
+    // console.log(value)
+    if (typeof value != "undefined") {
+      // condition ? if-true : if-false;
+     return value >= 100 ? '#d73027' :
+     		value >= 50 ? '#fdae61' :
+        value >= 0.0001 ? '#fee090' :
+        value >= 0.00 ? '#E5E5E5' :
+        value <= 0.0001 ? '#abd9e9' :
+        value <= -50.0 ? '#74add1' :
+             
+             '#4575b4' ;
+    } else {
+      return "gray"
+    }
+  };
+
+  var style = function(feature) {
+    var s = {
+      fillColor: getColor(feature.properties),
+      weight: 1.5,
+      opacity: .1,
+      color: '#000',
+      fillOpacity: 0.6
+    };
+    return s;
+  }
+
+  var geojson = L.geoJson(data, {
+    style: style,
+    onEachFeature: onEachFeature
+  }).addTo(map);
+
+}
+
+var onEachFeature = function(feature, layer) {
+  layer.bindPopup(ich.popup(feature.properties))
+};
+
+ map.scrollWheelZoom.disable();
